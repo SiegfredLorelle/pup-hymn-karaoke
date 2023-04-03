ba53b4d52b2910d7ab8eb948723ed76695815365
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-04-04 03:59:29 +0800

    add icons to replace play and volume icons. play buttons changes depending if it is on play, paused, or ended.

--- js/audio-auto-start.js
+++ js/audio-auto-start.js
@@ -1,27 +1,41 @@
 const lyrics = document.querySelector("#lyrics");
 const audioPlayer = document.querySelector("#audio-player");
+
 const playButton = document.querySelector("#play-button");
 const muteButton = document.querySelector("#mute-button");
+
+const playIcon = document.querySelector("#play-icon");
+const pauseIcon = document.querySelector("#pause-icon");
+const replayIcon = document.querySelector("#replay-icon");
+const unmuteIcon = document.querySelector("#unmute-icon");
+const muteIcon = document.querySelector("#mute-icon");
+
+const playIcons = [playIcon, pauseIcon, replayIcon]
+const volumeIcons = [unmuteIcon, muteIcon]
 
 let isIntroDone = false
 
 
 lyrics.stop();
+playIcon.style.visibility = "visible";
+unmuteIcon.style.visibility = "visible";
 
 playButton.addEventListener("click", () => {
     if (audioPlayer.paused) {
         audioPlayer.play();
         playLyrics();
+        ChangeIcon(pauseIcon, playIcons);
+
     }
     else {
         lyrics.stop();
         audioPlayer.pause();
+        ChangeIcon(playIcon, playIcons);
     }
 })
 
 
 function playLyrics() {
-    console.log(isIntroDone);
     if (isIntroDone) {
         lyrics.start();
     }
@@ -44,8 +58,19 @@ audioPlayer.addEventListener("ended", () => {
     lyrics.stop();
     isIntroDone = false;
     audioPlayer.addEventListener("timeupdate", detectLyricsStart);
+    ChangeIcon(replayIcon, playIcons);
 });
 
+function ChangeIcon(iconToActivate, iconGroup) {
+    for (icon of iconGroup) {
+        if (icon == iconToActivate) {
+            iconToActivate.style.visibility = "visible";
+        }
+        else {
+            icon.style.visibility = "hidden";
+        }
+    }
+}
 
 
 
