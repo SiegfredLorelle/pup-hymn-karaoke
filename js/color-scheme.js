98bc0bda55f3275bf6e32d19e97022ca3034e4d6
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-04-10 13:44:00 +0800

    clicking the color scheme button, changes the icon from light mode (sun) or dark mode (moon)

--- js/color-scheme.js
+++ js/color-scheme.js
@@ -1,7 +1,11 @@
 const root = document.querySelector(":root")
 const colorSchemeBtn = document.querySelector("#color-scheme-button");
 
+const darkModeIcon = document.querySelector("#dark-mode-icon");
+const lightModeIcon = document.querySelector("#light-mode-icon");
+
 let isDarkMode = true;
+let colors;
 
 let dark_mode_colors = {
     "--header-bg-color": "#2a2a30",
@@ -21,13 +25,7 @@ let light_mode_colors = {
 
 }
 
-// 
-// console.log(colorSchemeBtn);
-// console.log(getComputedStyle(root));
-
-
 colorSchemeBtn.addEventListener("click", () => {
-    let colors;
 
     if (isDarkMode) {
         colors = light_mode_colors;
@@ -37,11 +35,23 @@ colorSchemeBtn.addEventListener("click", () => {
         colors = dark_mode_colors;
         isDarkMode = true;
     }
+    changeColorSchemeIcon();
 
     for (key in colors) {
         root.style.setProperty(key, colors[key]);
     }
 });
+
+function changeColorSchemeIcon() {
+    if (isDarkMode) {
+        darkModeIcon.style.visibility = "hidden";
+        lightModeIcon.style.visibility = "visible";
+    }
+    else {
+        darkModeIcon.style.visibility = "visible";
+        lightModeIcon.style.visibility = "hidden";
+    }
+}
 
 
 
